0a222946b74273827bf34d7d8e7e008b47eb9ef0
DorukTaneli <45591689+DorukTaneli@users.noreply.github.com>
2019-06-16 21:32:12 +0300

    Add files via upload

---
+++ Lab2/Lab2-Part2/SComparator.vhd
@@ -0,0 +1,82 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:10:39 03/01/2018 
+-- Design Name: 
+-- Module Name:    SComparator - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity SComparator is
+    Port ( c3 : in  STD_LOGIC;
+           c2 : in  STD_LOGIC;
+           c1 : in  STD_LOGIC;
+           c0 : in  STD_LOGIC;
+           d3 : in  STD_LOGIC;
+           d2 : in  STD_LOGIC;
+           d1 : in  STD_LOGIC;
+           d0 : in  STD_LOGIC;
+           Gcd : out  STD_LOGIC;
+           E : out  STD_LOGIC;
+           Gdc : out  STD_LOGIC);
+end SComparator;
+
+architecture Behavioral of SComparator is
+		signal x3 : STD_LOGIC;
+		signal x2 : STD_LOGIC;
+		signal x1 : STD_LOGIC;
+		signal x0 : STD_LOGIC;
+		
+		signal nc3 : STD_LOGIC;
+		signal nc2 : STD_LOGIC;
+		signal nc1 : STD_LOGIC;
+		signal nc0 : STD_LOGIC;
+		
+		signal nd3 : STD_LOGIC;
+		signal nd2 : STD_LOGIC;
+		signal nd1 : STD_LOGIC;
+		signal nd0 : STD_LOGIC;
+begin
+		x3 <= c3 xnor d3;
+		x2 <= c2 xnor d2;
+		x1 <= c1 xnor d1;
+		x0 <= c0 xnor d0;
+		
+		nc3 <= not c3;
+		nc2 <= not c2;
+		nc1 <= not c1;
+		nc0 <= not c0;
+		
+		nd3 <= not d3;
+		nd2 <= not d2;
+		nd1 <= not d1;
+		nd0 <= not d0;
+		
+		E <= x3 and x2 and x1 and x0;
+		Gcd <= (nc3 and d3) or (x3 and c2 and nd2) or (x3 and x2 and c1 and nd1) or (x3 and x2 and x1 and c0 and nd0);
+		Gdc <= (c3 and nd3) or (x3 and nc2 and d2) or (x3 and x2 and nc1 and d1) or (x3 and x2 and x1 and nc0 and d0);
+
+end Behavioral;
+
---
+++ Lab2/Lab2-Part2/SComparatorSim.vhd
@@ -0,0 +1,118 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   15:50:06 03/01/2018
+-- Design Name:   
+-- Module Name:   G:/lab2part2/SComparatorSim.vhd
+-- Project Name:  lab2part2
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: SComparator
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY SComparatorSim IS
+END SComparatorSim;
+ 
+ARCHITECTURE behavior OF SComparatorSim IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT SComparator
+    PORT(
+         c3 : IN  std_logic;
+         c2 : IN  std_logic;
+         c1 : IN  std_logic;
+         c0 : IN  std_logic;
+         d3 : IN  std_logic;
+         d2 : IN  std_logic;
+         d1 : IN  std_logic;
+         d0 : IN  std_logic;
+         Gcd : OUT  std_logic;
+         E : OUT  std_logic;
+         Gdc : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal c3 : std_logic := '0';
+   signal c2 : std_logic := '0';
+   signal c1 : std_logic := '0';
+   signal c0 : std_logic := '0';
+   signal d3 : std_logic := '0';
+   signal d2 : std_logic := '0';
+   signal d1 : std_logic := '0';
+   signal d0 : std_logic := '0';
+
+ 	--Outputs
+   signal Gcd : std_logic;
+   signal E : std_logic;
+   signal Gdc : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   constant clock_period : time := 10 ns;
+ 
+BEGIN
+
+	-- Instantiate the Unit Under Test (UUT)
+   uut: SComparator PORT MAP (
+          c3 => c3,
+          c2 => c2,
+          c1 => c1,
+          c0 => c0,
+          d3 => d3,
+          d2 => d2,
+          d1 => d1,
+          d0 => d0,
+          Gcd => Gcd,
+          E => E,
+          Gdc => Gdc
+        );
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		c3 <= '0'; c2 <= '0'; c1 <= '0'; c0 <= '0';
+		c3 <= '0'; c2 <= '0'; d1 <= '0'; d0 <= '0';
+		wait for Clock_period;
+		c3 <= '0'; c2 <= '1'; c1 <= '0'; c0 <= '0';
+		d3 <= '0'; d2 <= '0'; d1 <= '0'; d0 <= '0';
+		wait for Clock_period;
+		c3 <= '0'; c2 <= '0'; c1 <= '0'; c0 <= '0';
+		d3 <= '0'; d2 <= '0'; d1 <= '1'; d0 <= '0';
+		wait for Clock_period;
+		c3 <= '1'; c2 <= '0'; c1 <= '0'; c0 <= '0';
+		d3 <= '0'; d2 <= '0'; d1 <= '0'; d0 <= '0';
+		wait for Clock_period;
+		c3 <= '0'; c2 <= '0'; c1 <= '0'; c0 <= '0';
+		d3 <= '1'; d2 <= '0'; d1 <= '0'; d0 <= '0';
+		wait for Clock_period;
+		c3 <= '0'; c2 <= '0'; c1 <= '0'; c0 <= '0';
+		d3 <= '0'; d2 <= '0'; d1 <= '0'; d0 <= '0';
+      wait;
+   end process;
+
+END;
